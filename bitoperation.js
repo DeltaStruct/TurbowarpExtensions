c02150b892e49d54b8a53c6a2bdd68e1c81fdbf2
DeltaStruct <DeltaStruct47@gmail.com>
2023-07-10 19:10:40 +0900

    Update bitoperation.js

--- bitoperation.js
+++ bitoperation.js
@@ -17,14 +17,42 @@ class HelloWorld {
             }
           }
         },
+        '---',
+        {
+          opcode: 'bitand',
+          blockType: Scratch.BlockType.REPORTER,
+          text: '[one] & [two]',
+          arguments: {
+            one: {
+              type: Scratch.ArgumentType.NUMBER
+            },
+            two: {
+              type: Scratch.ArgumentType.NUMBER
+            }
+          }
+        },
+        '---',
+        {
+          opcode: 'bitxor',
+          blockType: Scratch.BlockType.REPORTER,
+          text: '[one] | [two]',
+          arguments: {
+            one: {
+              type: Scratch.ArgumentType.NUMBER
+            },
+            two: {
+              type: Scratch.ArgumentType.NUMBER
+            }
+          }
+        },
         '---'
       ]
     };
   }
 
-  bitor(args) {
-    return args.one | args.two;
-  }
+  bitor(args) { return args.one | args.two; }
+  bitand(args) { return args.one & args.two; }
+  bitor(args) { return args.one ^ args.two; }
 }
 
 Scratch.extensions.register(new HelloWorld());
