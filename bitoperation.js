379aeb1f72edd4ff741b0c186380d471962788ab
DeltaStruct <DeltaStruct47@gmail.com>
2023-07-10 19:23:06 +0900

    Update bitoperation.js

--- bitoperation.js
+++ bitoperation.js
@@ -45,6 +45,56 @@ class HelloWorld {
             }
           }
         },
+        '---',
+        {
+          opcode: 'rshift',
+          blockType: Scratch.BlockType.REPORTER,
+          text: '[one] >> [two]',
+          arguments: {
+            one: {
+              type: Scratch.ArgumentType.NUMBER
+            },
+            two: {
+              type: Scratch.ArgumentType.NUMBER
+            }
+          }
+        },
+        '---',
+        {
+          opcode: 'lshift',
+          blockType: Scratch.BlockType.REPORTER,
+          text: '[one] << [two]',
+          arguments: {
+            one: {
+              type: Scratch.ArgumentType.NUMBER
+            },
+            two: {
+              type: Scratch.ArgumentType.NUMBER
+            }
+          }
+        },
+        '---',
+        {
+          opcode: 'lsb',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'LSB [IN]',
+          arguments: {
+            IN: {
+              type: Scratch.ArgumentType.NUMBER
+            }
+          }
+        },
+        '---',
+        {
+          opcode: 'bitnot',
+          blockType: Scratch.BlockType.REPORTER,
+          text: '~ [IN]',
+          arguments: {
+            IN: {
+              type: Scratch.ArgumentType.NUMBER
+            }
+          }
+        },
         '---'
       ]
     };
@@ -53,6 +103,10 @@ class HelloWorld {
   bitor(args) { return args.one | args.two; }
   bitand(args) { return args.one & args.two; }
   bitxor(args) { return args.one ^ args.two; }
+  rshift(args) { return (args.one >> args.two); }
+  lshift(args) { return (args.one << args.two); }
+  lsb(args) { return ((args.IN) & (-args.IN)); }
+  bitnot(args) { return (~args.IN); }
 }
 
 Scratch.extensions.register(new HelloWorld());
