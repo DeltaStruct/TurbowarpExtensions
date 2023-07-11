687c488807331961a9ee1fe964d4b1ef30e974ee
DeltaStruct <DeltaStruct47@gmail.com>
2023-07-11 11:59:35 +0900

    Update bitoperation.js

--- bitoperation.js
+++ bitoperation.js
@@ -153,8 +153,6 @@ class HelloWorld {
     let o = "";
     if (i==0) o = "0";
     while(i!=0){
-      console.log(i);
-      console.log(args.FORMAT);
       o += f.charAt(i%args.FORMAT);
       i=Math.floor(i/args.FORMAT);
     }
